89bbb7c3effab903cf2ceff71ad4c3050680cf32
Victor Alvarenga <info@victoralvarenga.com>
2021-12-18 18:32:21 -0300

    Feature: Add link navigation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Home: React.FC = () => {
-  return <div><h1>Home</h1></div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to="/show/1234">
+        <strong>Go to Show Page</strong>
+      </Link>
+    </div>
+  );
 }
--- src/pages/Show/index.tsx
+++ src/pages/Show/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Show: React.FC = () => {
-  return (<div><h1>Show</h1></div>);
+  return (
+    <div>
+      <h1>Show</h1>
+      <Link to="/">
+        <strong>Go to Home</strong>
+      </Link>
+    </div>
+  );
 }
