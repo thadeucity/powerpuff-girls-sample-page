e0b988933a00c4bf0781b3d5050cf6e3024a652e
Victor Alvarenga <info@victoralvarenga.com>
2021-12-21 01:09:50 -0300

    Feature: Filter episodes by season

--- src/pages/Episodes/Episodes.styles.ts
+++ src/pages/Episodes/Episodes.styles.ts
@@ -4,6 +4,38 @@ export const EpisodesPageContainer = styled.div`
   h1 {
     margin-bottom: 1.25rem;
   }
+
+  ul {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+
+    list-style: none;
+
+    margin-bottom: 2rem;
+
+    > * + * {
+      margin-left: 1rem;
+    }
+
+    button {
+      padding: 0.5rem 1rem;
+      border-radius: 0.5rem;
+      color: #fff;
+      background-color: var(--clr-text);
+      font-weight: bold;
+
+      transition: background 0.2s ease-in-out;
+
+      :hover {
+        background-color: var(--clr-primary);
+      }
+
+      &.selected {
+        background-color: var(--clr-primary);
+      }
+    }
+  }
 `;
 
 export const EpisodesContainer = styled.div`
